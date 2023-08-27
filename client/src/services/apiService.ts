f34ce1a565e206f5a1d608dd295398df794f4445
Arto Lahtinen <artolle@pm.me>
2023-08-27 15:17:31 +0200

    chore: tidy api service

--- client/src/services/apiService.ts
+++ client/src/services/apiService.ts
@@ -1,20 +1,8 @@
 const baseUrl = 'http://localhost:3001';
 
-// NOT USED!
-// // Retrieve all objects from the /models route
-// export async function getAll() {
-//   try {
-//     const response = await fetch(`${baseUrl}/models`);
-//     const allWeeObjects = await response.json();
-//     return allWeeObjects;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
 // Retrieve all objects of the specified
 // category from the /models/category route
-export async function getCategory(name = 'Default') {
+export async function getCategory (name = 'Default') {
   try {
     const response = await fetch(`${baseUrl}/models/category/${name}`);
     const categoryWeeObjects = await response.json();
